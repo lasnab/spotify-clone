4e73e2f877858dd2544b3660513cb9e5160a406d
lasnab <hrithikbansal70@gmail.com>
2020-10-08 19:32:27 +0530

    Updates

---
+++ precache-manifest.ea858ef7fa5f3ef366a9cd4db489e2a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5db7bbd5ba09f88e5426d1fb3cda62a",
+    "url": "/spotify-clone/index.html"
+  },
+  {
+    "revision": "ae51f1fcbe83890d4358",
+    "url": "/spotify-clone/static/css/main.d25f05c1.chunk.css"
+  },
+  {
+    "revision": "c7da96ccd333974290b2",
+    "url": "/spotify-clone/static/js/2.ce8b8c3e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/spotify-clone/static/js/2.ce8b8c3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae51f1fcbe83890d4358",
+    "url": "/spotify-clone/static/js/main.ab411273.chunk.js"
+  },
+  {
+    "revision": "83a75125f46b7cd87fb5",
+    "url": "/spotify-clone/static/js/runtime-main.c98e7f7b.js"
+  }
+]);
